d6a79a95f734830ba93b524a825319b44fe4b668
ndzf <ndzf.manifest@gmail.com>
2022-07-18 20:10:31 +0700

    Feat: Login

--- app/Controllers/AuthController.php
+++ app/Controllers/AuthController.php
@@ -17,6 +17,37 @@ class AuthController extends BaseController
 
     public function login()
     {
+        $validation = service("validation");
 
+        $data = [
+            "validation"        => $validation->getErrors(),
+        ];
+
+        return view("auth/login", $data);
+    }
+
+    public function attemptLogin()
+    {
+        $inputs = esc($this->request->getPost());
+
+        if (!$this->validate("attemptLogin")) {
+            return redirect()->to("/login")->withInput()->with("validationError", true);
+        }
+
+        $user = $this->userModel->getByUsername($inputs["username"]);
+        $hash = $user->password ?? "";
+        $passwordVerify = password_verify($inputs["password"], $hash);
+
+        if (empty($user) || !$passwordVerify) {
+            dd("Invalid username and password");
+        }
+
+        $payload = [
+            "isLoggedIn"        => true, 
+            "userID"            => $user->id,
+            "role"              => $user->role,
+        ];
+
+        session()->set($payload);
     }
 }
