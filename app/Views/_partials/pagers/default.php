f7f4d63a8bf4d039cbf3368cf8a00505ab61129b
ndzf <ndzf.manifest@gmail.com>
2022-07-19 19:00:14 +0700

    Fix: previous and next pager default

--- app/Views/_partials/pagers/default.php
+++ app/Views/_partials/pagers/default.php
@@ -2,11 +2,7 @@
 
 <nav aria-label="Page navigation">
     <ul class="pagination justify-content-end">
-        <li class="page-item">
-            <a href="<?= $pager->getFirst() ?>" aria-label="<?= lang('Pager.first') ?>" class="page-link">
-                <span aria-hidden="true"><?= lang('Pager.first') ?></span>
-            </a>
-        </li>
+        
         <li class="page-item">
             <a href="<?= $pager->getPrevious() ?>" aria-label="<?= lang('Pager.previous') ?>" class="page-link">
                 <span aria-hidden="true">&laquo;</span>
@@ -24,11 +20,6 @@
         <li class="page-item">
             <a href="<?= $pager->getNext() ?>" aria-label="<?= lang('Pager.next') ?>" class="page-link">
                 <span aria-hidden="true">&raquo;</span>
-            </a>
-        </li>
-        <li class="page-item">
-            <a href="<?= $pager->getLast() ?>" aria-label="<?= lang('Pager.last') ?>" class="page-link">
-                <span aria-hidden="true"><?= lang('Pager.last') ?></span>
             </a>
         </li>
     </ul>
