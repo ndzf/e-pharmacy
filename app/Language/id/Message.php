e6b786676fd389a7a2ad10180a3c2bd87368448f
ndzf <ndzf.manifest@gmail.com>
2022-07-21 16:32:33 +0700

    Add: Localiztion Message for Indonesia Language

---
+++ app/Language/id/Message.php
@@ -0,0 +1,12 @@
+<?php
+
+return [
+    "success"               => [
+        "create"            => "Berhasil menambah {0}",
+        "update"            => "Berhasil memperbaharui {0}",
+        "delete"            => "Berhasil menghapus {0}",
+    ],
+    "error"                 => [
+        "notFound"          => "{0} Tidak Ditemukan",
+    ]
+];
