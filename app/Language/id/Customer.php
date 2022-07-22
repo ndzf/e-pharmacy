5557164d65177955999d92da7432b1313c531e0e
ndzf <ndzf.manifest@gmail.com>
2022-07-22 11:15:26 +0700

    Add: localization customers for Indonesian language

---
+++ app/Language/id/Customer.php
@@ -0,0 +1,24 @@
+<?php
+
+return [
+    "name"              => "Nama Pelanggan",
+    "phoneNumber"       => "No. Tlp",
+    "email"             => "Email",
+    "role"              => "Role",
+    "address"           => "Alamat",
+    "save"              => "Simpan",
+    
+    "title"             => [
+        "index"         => "Data Pelanggan",
+        "create"        => "Tambah Data Pelanggan",
+        "edit"          => "Edit Data Pelanggan",
+        "delete"        => "Hapus Data Pelanggan",
+    ],
+
+    "roles"             => [
+        "customer"      => "Pelanggan",
+        "member"        => "Member",
+        "reseller"      => "Reseller"
+    ],
+
+];
