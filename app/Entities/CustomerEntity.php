0dc39559252f3d54ccee7314821ac13d477ea3f0
ndzf <ndzf.manifest@gmail.com>
2022-07-22 11:31:42 +0700

    Add: getBadgeTheme method

--- app/Entities/CustomerEntity.php
+++ app/Entities/CustomerEntity.php
@@ -9,4 +9,18 @@ class CustomerEntity extends Entity
     protected $datamap = [];
     protected $dates   = ['created_at', 'updated_at'];
     protected $casts   = [];
+
+    public function getBadgeTheme() {
+        if ($this->role == "customer") {
+            return "badge-light-warning";
+        }
+
+        if ($this->role == "member") {
+            return "badge-light-primary";
+        }
+
+        if ($this->role == "reseller") {
+            return "badge-light-success";
+        }
+    }
 }
