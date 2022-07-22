7d2979c55818b53c95b4aefef695bf97b75c6def
ndzf <ndzf.manifest@gmail.com>
2022-07-22 09:14:54 +0700

    Add: CustomerSeeder

---
+++ app/Database/Seeds/CustomerSeeder.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Database\Seeds;
+
+use CodeIgniter\Database\Seeder;
+
+class CustomerSeeder extends Seeder
+{
+    public function run()
+    {
+        $customers = [
+            [
+                "name"          => "Mamat manja",
+                "phone_number"  => "620123908",
+                "email"         => "",
+                "address"       => "Kendal City",
+                "role"          => "customer",
+            ],
+            [
+                "name"          => "Anthony Martial",
+                "phone_number"  => "620123908",
+                "email"         => "",
+                "address"       => "Franc",
+                "role"          => "members",
+            ],
+            [
+                "name"          => "Donny Van De Beek",
+                "phone_number"  => "620123908",
+                "email"         => "donny@gmail.com",
+                "address"       => "Holland",
+                "role"          => "reseller",
+            ],
+        ];
+
+        $this->db->table("customers")->insertBatch($customers);
+    }
+}
