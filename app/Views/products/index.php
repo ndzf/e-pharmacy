2a81d5ceaf037dcd2c2f7e7b77e510b86c52e325
ndzf <ndzf.manifest@gmail.com>
2022-07-23 15:45:03 +0700

    Add: index view for products

---
+++ app/Views/products/index.php
@@ -0,0 +1,18 @@
+<?= $this->extend("layouts/main") ?>
+
+<?= $this->section("content")  ?>
+
+<div class="container-fluid">
+    <div class="row">
+        <div class="col-12">
+            <div class="card border-0 rounded-6 shadow">
+                <div class="card-header border-0 rounded-6 py-3 bg-white d-flex">
+
+                </div>
+                <div class="card-body pt-0"></div>
+            </div>
+        </div>
+    </div>
+</div>
+
+<?= $this->endSection()  ?>
