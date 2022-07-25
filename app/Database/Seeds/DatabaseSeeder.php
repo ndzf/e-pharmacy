0b449b463b611a96f15e005fd7e8a352dfe5a3eb
ndzf <ndzf@manifest@gmail.com>
2022-07-25 17:30:35 +0700

    Add: DatabaseSeeder

---
+++ app/Database/Seeds/DatabaseSeeder.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Database\Seeds;
+
+use CodeIgniter\Database\Seeder;
+
+class DatabaseSeeder extends Seeder
+{
+    public function run()
+    {
+        $this->call("UserSeeder");
+        $this->call("CategorySeeder");
+        $this->call("SupplierSeeder");
+        $this->call("CustomerSeeder");
+    }
+}
