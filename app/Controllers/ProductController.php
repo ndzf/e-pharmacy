0e896d1a7c1236593f6d1c9e29d9ef64e9e5776a
ndzf <ndzf.manifest@gmail.com>
2022-07-29 19:34:56 +0700

    Add: create() method on ProductController

--- app/Controllers/ProductController.php
+++ app/Controllers/ProductController.php
@@ -25,7 +25,7 @@ class ProductController extends BaseController
             "type"              => esc($this->request->getVar("type")),
             "category"          => esc($this->request->getVar("category")),
         ];
-
+        $validation = service("validation");
         $products = $this->productModel->search($inputs["keyword"], $inputs["type"], $inputs["category"]);
 
         $data = [
@@ -33,8 +33,25 @@ class ProductController extends BaseController
             "inputs"        => $inputs,
             "products"      => $products->paginate(10, "products"),
             "pager"         => $products->pager->links("products", "default"),
+            "validation"    => $validation->getErrors(),
         ];
 
         return view("products/index", $data);
+    }
+
+    public function create()
+    {
+        $inputs = esc($this->request->getPost());
+
+        if (!$this->validate("createProduct")) {
+            return redirect()->to("/products")->withInput()->with("validationErrorCreate", true);
+        }
+
+        try {
+            $this->productModel->create($inputs);
+            return redirect()->to("/products")->with("successMessage", lang("Message.success.create", [strtolower($this->title)]));
+        } catch(\Exception $e) {
+            return redirect()->to("/products")->with("errorMessage", $e->getMessage());
+        }
     }
 }
