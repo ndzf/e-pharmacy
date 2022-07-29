36f01a2442c37356619c94fc786d499a6960a0ac
ndzf <ndzf.manifest@gmail.com>
2022-07-29 15:31:12 +0700

    Add: index() method on ProductController

--- app/Controllers/ProductController.php
+++ app/Controllers/ProductController.php
@@ -20,6 +20,21 @@ class ProductController extends BaseController
 
     public function index()
     {
+        $inputs = [
+            "keyword"           => esc($this->request->getVar("q")),
+            "type"              => esc($this->request->getVar("type")),
+            "category"          => esc($this->request->getVar("category")),
+        ];
 
+        $products = $this->productModel->search($inputs["keyword"], $inputs["type"], $inputs["category"]);
+
+        $data = [
+            "title"         => $this->title,
+            "inputs"        => $inputs,
+            "products"      => $products->paginate(10, "products"),
+            "pager"         => $products->pager->links("products", "default"),
+        ];
+
+        return view("products/index", $data);
     }
 }
