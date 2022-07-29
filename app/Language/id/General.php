99a610501674a0c82a5c4546375650d51603208b
ndzf <ndzf.manifest@gmail.com>
2022-07-29 15:31:53 +0700

    Add: Localization for Indonesian language

---
+++ app/Language/id/General.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    "save"                      => "Simpan",
+    "apply"                     => "Terpakan",
+    "search"                    => "Cari...",
+    "filter"                    => "Filter",
+    "reset"                     => "Reset",
+];
---
+++ app/Language/id/Product.php
@@ -0,0 +1,21 @@
+<?php
+
+return [
+    "name"                  => "Nama",
+    "supplier"              => "Supplier",
+    "category"              => "Kategori",
+    "type"                  => "Tipe",
+    "qty"                   => "Qty",
+    "minimumQty"            => "Minimum Qty",
+    "originalPrice"         => "Harga Asli",
+    "sellingPrice"          => "Harga Jual",
+    "memberPrice"           => "Harga Member",
+    "wholesalePrice"        => "Harga Jual",
+
+    "title"                 => [
+        "index"             => "Data Produk",
+        "create"            => "Tambah data produk",
+        "edit"              => "Edit data produk",
+        "delete"            => "Hapus data produk",
+    ]
+];
