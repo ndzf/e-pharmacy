10cb495e46542ba579c027366e49551f914c9b5d
ndzf <ndzf.manifest@gmail.com>
2022-08-01 23:18:10 +0700

    Fix: call ProductSeeder

--- app/Database/Seeds/DatabaseSeeder.php
+++ app/Database/Seeds/DatabaseSeeder.php
@@ -12,5 +12,6 @@ class DatabaseSeeder extends Seeder
         $this->call("CategorySeeder");
         $this->call("SupplierSeeder");
         $this->call("CustomerSeeder");
+	$this->call("ProductSeeder");
     }
 }
