d2d9d4fc2d49392e0d0f35f0f73f4c0aae684f0e
ndzf <ndzf.manifest@gmail.com>
2022-08-03 17:36:13 +0700

    Add: TransactionMigration

---
+++ app/Database/Migrations/2022-08-03-094921_TransactionsMigration.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Database\Migrations;
+
+use CodeIgniter\Database\Migration;
+
+class TransactionsMigration extends Migration
+{
+    public function up()
+    {
+		$this->forge->addField([
+			"id"				=> ["type" => "int", "constraint" => 11, "auto_increment" => 11],
+			"user_id"			=> ["type" => "int", "constraint" => 11],
+			"customer_id"		=> ["type" => "int", "constraint" => 11, "null" => true],
+			"status"			=> ["type" => "enum", "constraint" => ["open", "done"]],
+			"payment_status"	=> ["type" => "enum", "constraint" => ["cash", "debt"]],
+			"grand_total"		=> ["type" => "int", "constraint" => 11, "null" => true],
+			"discount"			=> ["type" => "int", "constraint" => 11, "null" => true],
+			"date"				=> ["type" => "date"],
+			"created_at"		=> ["type" => "timestamp"],
+			"updated_at"		=> ["type" => "timestamp", "null" => true],
+		]);
+		$this->forge->addkey("id", true);
+		$this->forge->createTable("transactions", true);
+    }
+
+    public function down()
+    {
+		$this->forge->dropTable("transactions", true);
+    }
+}
