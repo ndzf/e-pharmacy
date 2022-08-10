eecd8ba72ca8ecc4a31a3fdee2ad2088c949c117
ndzf <ndzf.manifest@gmail.com>
2022-08-10 15:00:06 +0700

    Add: PurchasesMigration

---
+++ app/Database/Migrations/2022-08-10-075353_PurchasesMigration.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Database\Migrations;
+
+use CodeIgniter\Database\Migration;
+
+class PurchasesMigration extends Migration
+{
+    public function up()
+    {
+        $this->forge->addField([
+            "id"                => ["type" => "int", "constraint" => 11, "auto_increment" => true],
+            "user_id"           => ["type" => "int", "constraint" => 11],
+            "status"            => ["type" => "enum", "constraint" => ["open", "done"], "default" => "open"],
+            "payment_status"    => ["type" => "enum", "constraint" => ["cash", "debt"], "null" => true],
+            "grand_total"       => ["type" => "int", "constraint" => 11, "null" => true],
+            "date"              => ["type" => "date"],
+            "created_at"        => ["type" => "timestamp"],
+            "updated_at"        => ["type" => "timestamp", "null" => true],
+        ]);
+
+        $this->forge->addKey("id", true);
+        $this->forge->createTable("purchases");
+    }
+
+    public function down()
+    {
+        $this->forge->dropTable("purchases");
+    }
+}
