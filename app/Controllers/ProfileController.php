81726d42cd8a99b93c9bf19be8e397569a00c23b
ndzf <ndzf.manifest@gmail.com>
2022-08-15 16:03:18 +0700

    Add: ProfileController:

---
+++ app/Controllers/ProfileController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Controllers;
+
+use App\Controllers\BaseController;
+use App\Models\UserModel;
+
+class ProfileController extends BaseController
+{
+    protected $userModel;
+
+    public function __construct()
+    {
+        $this->userModel = new UserModel();
+    }
+
+    public function index()
+    {
+        //
+    }
+}
