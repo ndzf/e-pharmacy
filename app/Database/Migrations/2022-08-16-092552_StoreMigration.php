33e9b3f823db96bf10f0dd57f731cab16f5d3341
ndzf <ndzf.manifest@gmail.com>
2022-08-16 16:30:25 +0700

    Add: StoreMigration

---
+++ app/Database/Migrations/2022-08-16-092552_StoreMigration.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Database\Migrations;
+
+use CodeIgniter\Database\Migration;
+
+class StoreMigration extends Migration
+{
+    public function up()
+    {
+        $this->forge->addField([
+            "id"                    => ["type" => "int", "constraint" => 11, "auto_increment" => true],
+            "name"                  => ["type" => "varchar", "constraint" => 255],
+            "address"               => ["type" => "text", "null" => true],
+            "phone_number"          => ["type" => "varchar", "constraint" => 11, "null" => true],
+            "email"                 => ["type" => "varchar", "constraint" => 100, "null" => true],
+        ]);
+        $this->forge->addKey("id", true);
+        $this->forge->createTable("store");
+    }
+
+    public function down()
+    {
+        $this->forge->dropTable("store");
+    }
+}
