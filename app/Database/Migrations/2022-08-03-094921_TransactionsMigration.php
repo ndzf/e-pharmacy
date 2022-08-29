70412cc4d0e11dd22a4b66a5f57ea0c8e5aab70d
ndzf <ndzf.manifest@gmail.com>
2022-08-29 15:38:37 +0700

    Add: bunch of field

--- app/Database/Migrations/2022-08-03-094921_TransactionsMigration.php
+++ app/Database/Migrations/2022-08-03-094921_TransactionsMigration.php
@@ -6,8 +6,8 @@ use CodeIgniter\Database\Migration;
 
 class TransactionsMigration extends Migration
 {
-    public function up()
-    {
+	public function up()
+	{
 		$this->forge->addField([
 			"id"				=> ["type" => "int", "constraint" => 11, "auto_increment" => true],
 			"user_id"			=> ["type" => "int", "constraint" => 11],
@@ -16,16 +16,20 @@ class TransactionsMigration extends Migration
 			"payment_status"	=> ["type" => "enum", "constraint" => ["cash", "debt"], "null" => true],
 			"grand_total"		=> ["type" => "int", "constraint" => 11, "null" => true],
 			"discount"			=> ["type" => "int", "constraint" => 11, "null" => true],
+			"facet"				=> ["type" => "varchar", "constraint" => 255, "null" => true],
+			"pick_up_date"		=> ["type" => "date", "null" => true],
+			"pd"				=> ["type" => "varchar", "constraint" => 255, "null" => true],
+			"note"				=> ["type" => "text", "null" => true],
 			"date"				=> ["type" => "date"],
 			"created_at"		=> ["type" => "timestamp"],
 			"updated_at"		=> ["type" => "timestamp", "null" => true],
 		]);
 		$this->forge->addkey("id", true);
 		$this->forge->createTable("transactions", true);
-    }
+	}
 
-    public function down()
-    {
+	public function down()
+	{
 		$this->forge->dropTable("transactions", true);
-    }
+	}
 }
