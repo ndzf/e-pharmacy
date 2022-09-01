dcb39854e4b801e57f65796d6e2862b56075d131
ndzf <ndzf.manifest@gmail.com>
2022-09-01 15:19:22 +0700

    Add: CustomerCardSettingModel and CustomerCardSettingEntity

---
+++ app/Entities/CustomerCardSettingEntity.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Entities;
+
+use CodeIgniter\Entity\Entity;
+
+class CustomerCardSettingEntity extends Entity
+{
+    protected $datamap = [];
+    protected $dates   = [];
+    protected $casts   = [];
+}
