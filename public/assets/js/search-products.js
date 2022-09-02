e4266f7e7569d250e1be804d9e8b9a0d45f982c2
ndzf <ndzf.manifest@gmail.com>
2022-09-02 10:53:23 +0700

    Fix: show sph, cyl, add when lens_type == regular

--- public/assets/js/search-products.js
+++ public/assets/js/search-products.js
@@ -1,5 +1,6 @@
 function getCard(product) {
     let card = "";
+    console.log(product);
     card += `<div class="mb-3">`;
     card += `<a href="javascript:void(0)" onclick="addProduct(${product.id})">`;
     card += `<div class="card border-0 shadow">`;
@@ -18,18 +19,28 @@ function getCard(product) {
     card += `</tr>`;
     card += `</thead>`;
     card += `<tbody class="text-gray-700 fw-500">`;
-    card += `<tr>`;
-    card += `<td class="text-primary">R</td>`;
-    card += `<td>${product.r_sph}</td>`;
-    card += `<td>${product.r_cyl}</td>`;
-    card += `<td>${product.r_add}</td>`;
-    card += `</tr>`;
-    card += `<tr>`;
-    card += `<td class="text-primary">L</td>`;
-    card += `<td>${product.l_sph}</td>`;
-    card += `<td>${product.l_cyl}</td>`;
-    card += `<td>${product.l_add}</td>`;
-    card += `</tr>`;
+    if (product.lens_type == "progressive") {
+        card += `<tr>`;
+        card += `<td class="text-primary">R</td>`;
+        card += `<td>${product.r_sph}</td>`;
+        card += `<td>${product.r_cyl}</td>`;
+        card += `<td>${product.r_add}</td>`;
+        card += `</tr>`;
+        card += `<tr>`;
+        card += `<td class="text-primary">L</td>`;
+        card += `<td>${product.l_sph}</td>`;
+        card += `<td>${product.l_cyl}</td>`;
+        card += `<td>${product.l_add}</td>`;
+        card += `</tr>`;
+    }
+    if (product.lens_type == "regular") {
+        card += `<tr>`;
+        card += `<td class="text-primary"></td>`;
+        card += `<td>${product.sph}</td>`;
+        card += `<td>${product.cyl}</td>`;
+        card += `<td>${product.add}</td>`;
+        card += `</tr>`;
+    }
     card += `</tbody>`;
     card += `</table>`;
     card += `</div>`;
